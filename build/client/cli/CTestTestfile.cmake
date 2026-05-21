c13b83de13ea532f1931973569d063f0cfcc42e9
zzzyouzzz <3104926397@qq.com>
2026-05-21 16:50:30 +0800

    feat: add .gitignore

--- build/client/cli/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/c/vsc_pro/task3/client/cli
-# Build directory: D:/c/vsc_pro/task3/build/client/cli
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/CMakeFiles/ServerTests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ServerTests.
-# This may be replaced when dependencies are built.
